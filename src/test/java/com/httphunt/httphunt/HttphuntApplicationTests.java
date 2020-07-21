650b8b726c92a13bbc35eb19244e7cf1f44c8396
Goodness Daramola <gdaramol@thoughtworks.com>
2020-07-21 12:16:26 -0500

    init

---
+++ src/test/java/com/httphunt/httphunt/HttphuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.httphunt.httphunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttphuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
